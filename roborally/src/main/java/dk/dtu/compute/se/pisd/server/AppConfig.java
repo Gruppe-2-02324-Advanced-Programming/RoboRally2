4321ef030c7637bd6f98c357016d334afba24dfa
Setare Izadi <s232629@dtu.dk>
2024-06-11 19:47:40 +0200

    configuration app

--- roborally/src/main/java/dk/dtu/compute/se/pisd/server/AppConfig.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/server/AppConfig.java
@@ -1,4 +1,8 @@
 package dk.dtu.compute.se.pisd.server;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
 }
