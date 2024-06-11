a67f84bac82f2919c3bfce503e63d86ea0e2f4e2
Setare Izadi <s232629@dtu.dk>
2024-06-11 21:11:05 +0200

    Tilføjet til model, men ikke færdig

--- roborally/src/main/java/dk/dtu/compute/se/pisd/server/model/Board.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/server/model/Board.java
@@ -1,4 +1,48 @@
 package dk.dtu.compute.se.pisd.server.model;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class Board {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer gameId;
+
+    @Column(nullable = false)
+    private int width;
+
+    @Column(nullable = false)
+    private int height;
+
+    @Column(nullable = false)
+    private String boardName;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Space> spaces = new ArrayList<>();
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Player> players = new ArrayList<>();
+
+    @ManyToOne
+    private Player current;
+
+    @Enumerated(EnumType.STRING)
+    private Phase phase = Phase.INITIALISATION;
+
+    private int step = 0;
+    private boolean stepMode;
+    private int totalCheckpoints = 0;
+    private int counter;
+    private boolean won = false;
+
 }
---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/server/model/Phase.java
@@ -0,0 +1,5 @@
+package dk.dtu.compute.se.pisd.server.model;
+
+public class Phase {
+    public static final Phase INITIALISATION = new Phase();
+}
