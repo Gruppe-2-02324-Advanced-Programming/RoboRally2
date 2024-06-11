4fa6edb595420b18731ed63d7768aabbaed94cff
Christoffer Fink <christo.fink.1@gmail.com>
2024-06-11 10:12:48 +0200

    Springboot

--- roborally/src/main/java/dk/dtu/compute/se/pisd/server/TestApplication.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/server/TestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoborallyApplication {
+public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoborallyApplication.class, args);
+		SpringApplication.run(TestApplication.class, args);
 	}
 
 }
