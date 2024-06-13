fd39610951bb10634d1c9040e509fe0c2fd699b2
Setare Izadi <s232629@dtu.dk>
2024-06-13 11:18:51 +0200

    mere

---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/server/model/Game.java
@@ -0,0 +1,27 @@
+package dk.dtu.compute.se.pisd.server.model;
+import dk.dtu.compute.se.pisd.server.model.Player;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "games")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_id")
+    private List<Player> players;
+
+}
---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/server/repository/GameRepository.java
@@ -0,0 +1,8 @@
+package dk.dtu.compute.se.pisd.server.repository;
+import dk.dtu.compute.se.pisd.server.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
