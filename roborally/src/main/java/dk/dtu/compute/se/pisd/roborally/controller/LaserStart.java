4465e17e683d0987d39a560b0a3dee387fefaf8b
Christoffer Fink <christo.fink.1@gmail.com>
2024-06-19 22:42:33 +0200

    VISUALS og placeholders

---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/LaserStart.java
@@ -0,0 +1,50 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+import org.jetbrains.annotations.NotNull;
+import javax.swing.JOptionPane;
+
+/** This class represents the pit field action.
+ *  When a player lands on a pit, it shows a popup message.
+ *  @author Christoffer s205449
+ *
+ */
+public class LaserStart extends FieldAction {
+
+    private Heading heading;
+
+    /**
+     * Constructor for the laser start with a specified heading.
+     * @param heading the heading direction of the laser start
+     */
+    public LaserStart(Heading heading) {
+        this.heading = heading;
+    }
+
+
+    /**
+     * Gets the heading direction of the laser start.
+     * @return the heading direction
+     */
+    public Heading getHeading() {
+        return heading;
+    }
+
+    /**
+     * Sets the heading direction of the laser start.
+     * @param heading the heading direction to set
+     */
+    public void setHeading(Heading heading) {
+        this.heading = heading;
+    }
+
+    /**
+     * this method does nothing purely for cosmetic
+     */
+    @Override
+    public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
+        return false;
+    }
+}
