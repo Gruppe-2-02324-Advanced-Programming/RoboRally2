da025077c630e621dcb88931fdb60605876cf745
Setare <seti@dtu.dk>
2024-06-26 13:00:10 +0200

    tilføjet flere unittests

--- roborally/src/test/test/GameControllerTest.java
+++ roborally/src/test/test/GameControllerTest.java
@@ -24,6 +24,7 @@ class GameControllerTest {
         Platform.startup(latch::countDown);
         latch.await();
     }
+
     @BeforeEach
     void setUp() {
         Board board = new Board(TEST_WIDTH, TEST_HEIGHT);
@@ -44,6 +45,7 @@ class GameControllerTest {
 
     /**
      * Testing if player moves forward when supposed to
+     *
      * @author Christoffer Fink s205449
      * Testing if player moves forward when supposed to
      */
@@ -61,8 +63,10 @@ class GameControllerTest {
         Assertions.assertNull(board.getSpace(0, 0).getPlayer(),
                 "Space (0,0) should be empty!");
     }
+
     /**
      * Same as moveForward test just checking if twice
+     *
      * @author Christoffer Fink s205449
      */
     @Test
@@ -84,6 +88,7 @@ class GameControllerTest {
 
     /**
      * Same as moveForward test just checking if it performs the function three times
+     *
      * @author Setare Izadi, s232629
      */
     @Test
@@ -104,12 +109,12 @@ class GameControllerTest {
 
 
     /**
-     *@author Christoffer, s205449
+     * @author Christoffer, s205449
      * turnRight test
-     *against west, because start pos is SOUTH
+     * against west, because start pos is SOUTH
      */
     @Test
-    void turnRight(){
+    void turnRight() {
         Board board = gameController.board;
         Player player = board.getCurrentPlayer();
         gameController.turnRight(player);
@@ -117,8 +122,9 @@ class GameControllerTest {
     }
 
     /**
-     *  turnLeft test
-     *  against east ~~~
+     * turnLeft test
+     * against east ~~~
+     *
      * @author Christoffer s205449
      */
     @Test
@@ -132,15 +138,15 @@ class GameControllerTest {
 
     /**
      * Testing if the player makes an uturn when supposed to
-     * @author Christoffer Fink s205449
      *
+     * @author Christoffer Fink s205449
      */
     @Test
     void uTurn() {
         Board board = gameController.board;
         Player player = board.getCurrentPlayer();
         gameController.uTurn(player);
-        Assertions.assertEquals(Heading.NORTH, player.getHeading(),"player should be heading NORTH" );
+        Assertions.assertEquals(Heading.NORTH, player.getHeading(), "player should be heading NORTH");
         Assertions.assertEquals(player, board.getSpace(0, 0).getPlayer(),
                 "Player " + player.getName() + "space should be (0,0)");
 
@@ -148,12 +154,13 @@ class GameControllerTest {
 
     /**
      * Testing again command card by setting up a player with a forward and again card in their programmingfield
+     *
      * @Author Jacob, s164958
      * @Param player
      * @Return void
      */
     @Test
-    void testAgain(){
+    void testAgain() {
         Board board = gameController.board;
         Player player = board.getCurrentPlayer();
         CommandCard fwd = new CommandCard(Command.FORWARD);
@@ -169,6 +176,7 @@ class GameControllerTest {
 
     /**
      * Test to ensure timer decreases over time and transitions correctly at zero.
+     *
      * @Author Setare Izadi, s232629
      */
     @Test
@@ -186,6 +194,7 @@ class GameControllerTest {
 
     /**
      * Test player movement is blocked by a wall.
+     *
      * @Author Setare Izadi, s232629
      */
     @Test
@@ -205,6 +214,7 @@ class GameControllerTest {
 
     /**
      * Test command execution for non-movement commands.
+     *
      * @Author Setare Izadi, s232629
      */
     @Test
@@ -218,6 +228,71 @@ class GameControllerTest {
 
         Assertions.assertEquals(initialEnergy + 1, player.getEnergyCubes(),
                 "Player should have one more energy cube after executing POWER_UP command.");
+    }
+
+    /**
+     * Test the spam command to ensure the card is replaced correctly.
+     *
+     * @autor Setare Izadi, s232629
+     */
+    @Test
+    void testSpamCommand() {
+        Board board = gameController.board;
+        Player player = board.getCurrentPlayer();
+        CommandCard spamCard = new CommandCard(Command.SPAM);
+
+        player.getProgramField(board.getStep()).setCard(spamCard);
+        gameController.spam(player);
+
+        Assertions.assertNotEquals(spamCard, player.getProgramField(board.getStep()).getCard(),
+                "The card in the program field should be replaced by a new card from the draw pile.");
+    }
+
+    /**
+     * Test the cleanup method to ensure program fields are cleaned up correctly.
+     *
+     * @autor Setare Izadi, s232629
+     */
+    @Test
+    void testCleanup() {
+        Board board = gameController.board;
+        Player player = board.getCurrentPlayer();
+        CommandCard commandCard = new CommandCard(Command.FORWARD);
+
+        for (int i = 0; i < Player.NO_REGISTERS; i++) {
+            player.getProgramField(i).setCard(commandCard);
+        }
+
+        gameController.cleanup();
+
+        for (int i = 0; i < Player.NO_REGISTERS; i++) {
+            Assertions.assertNull(player.getProgramField(i).getCard(),
+                    "The program field should be empty after cleanup.");
+            Assertions.assertTrue(player.getDiscardpile().getCards().contains(commandCard),
+                    "The discard pile should contain the command card after cleanup.");
+        }
+    }
+
+    /**
+     * Test the backup command to ensure the player moves backward correctly.
+     *
+     * @autor Setare Izadi, s232629
+     */
+    @Test
+    void testBackup() {
+        Board board = gameController.board;
+        Player player = board.getCurrentPlayer();
+        player.setHeading(Heading.SOUTH); // Ensure player is facing south initially
+        player.setSpace(board.getSpace(0, 1)); // Place player in position (0, 1)
+
+        gameController.backUp(player);
+
+        Assertions.assertEquals(player, board.getSpace(0, 0).getPlayer(),
+                "Player " + player.getName() + " should be in space (0,0)!");
+        Assertions.assertEquals(Heading.SOUTH, player.getHeading(),
+                "Player should be heading SOUTH!");
+        Assertions.assertNull(board.getSpace(0, 1).getPlayer(),
+                "Space (0,1) should be empty!");
     }
 }
 
